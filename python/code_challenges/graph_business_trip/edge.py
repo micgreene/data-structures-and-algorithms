baacf4ea8261681ba436a7cf129ddc74b7a5bf60
micgreene <mgreene11111@yahoo.com>
2022-04-07 14:46:45 -0700

    added empty python files, copied in files necessary for graph data structure, added empty README.md file, added empty testing file.

---
+++ python/code_challenges/graph_business_trip/edge.py
@@ -0,0 +1,14 @@
+class Edge():
+    '''
+    Class which tracks directed edges in a graph between two vertices, and their weight if one was given.
+    '''
+    def __init__(self, start_vert, end_vert=None, weight=0):
+        self.start_vert = start_vert
+        self.end_vert =end_vert
+        self.weight = weight
+
+    def __str__(self):
+        return f'an edge between {self.start_vert} and {self.end_vert}, with a weight of {self.weight}'
+
+    def __repr__(self):
+        return f'Edge({self.start_vert, self.end_vert, self.weight})'
